01b3e9ee25609c3cfd56ad4d1f6ee4262df1a4a9
CalebVanEe <VanEeCM23@gcc.edu>
2025-11-05 18:18:33 -0500

    Edited loading screen and added Main Menu music

--- Assets/Scripts/LoadingSceneManagerScript.cs
+++ Assets/Scripts/LoadingSceneManagerScript.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LoadingSceneManagerScript : MonoBehaviour
 {
-    public string Level1Message = "You are in the depths of the prison in the torture chamber. You must find your way out to survive";
+    public string Level1FirstMessage = "Your name is Dmitri. You have been wrongfully imprisoned in a Russian prison for the past 10 years";
+    public string Level1SecondMessage = "You have finally had enough and decide to escape, but it won't be easy";
+    public string Level1ThirdMessage = "You are in the depths of the prison in the torture chamber. You must find your way out to survive";
     public string Level2Message = "You've escaped the Torture Chamber, and find yourself locked in the Warden's Office";
     public string Level3Message = "You've made your way out to the prison yard. The guards have discovered your absence and are looking for you. Escape or die";
     public string CompletionMessage = "You finally escaped the Russian Prison and reunited with your family after 10 years of imprisonment";
     public int levelIndex;
     public TMP_Text messageText;
-    private string currentMessage;
 
     void Start()
     {
@@ -22,50 +23,68 @@ public class LoadingSceneManagerScript : MonoBehaviour
     {
         if (levelIndex == 1)
         {
-            currentMessage = Level1Message;
-            Invoke("LoadLevel1", 6f);
+            StartCoroutine(DisplayLevel1Messages());
         }
         else if (levelIndex == 2)
         {
-            currentMessage = Level2Message;
-            Invoke("LoadLevel2", 5f);
+            StartCoroutine(TypeText(Level2Message));
+            Invoke("LoadLevel2", 6f);
         }
         else if (levelIndex == 3)
         {
-            currentMessage = Level3Message;
-            Invoke("LoadLevel3", 7f);
+            StartCoroutine(TypeText(Level3Message));
+            Invoke("LoadLevel3", 8f);
         }
         else if (levelIndex == 4)
         {
-            currentMessage = CompletionMessage;
+            StartCoroutine(TypeText(CompletionMessage));
             Invoke("LoadMainMenu", 10f);
         }
-        // Start the coroutine to display text character by character
-        StartCoroutine(TypeText(currentMessage));
+    }
+
+    IEnumerator DisplayLevel1Messages()
+    {
+        // Display first message
+        yield return StartCoroutine(TypeText(Level1FirstMessage));
+        yield return new WaitForSeconds(2f); // 1 second delay
+
+        // Display second message
+        yield return StartCoroutine(TypeText(Level1SecondMessage));
+        yield return new WaitForSeconds(2f); // 1 second delay
+
+        // Display third message
+        yield return StartCoroutine(TypeText(Level1ThirdMessage));
+
+        // Wait a bit after the last message, then load the level
+        yield return new WaitForSeconds(2f);
+        LoadLevel1();
     }
 
     IEnumerator TypeText(string message)
     {
         messageText.text = ""; // Clear the text first
-
         foreach (char letter in message)
         {
             messageText.text += letter;
             yield return new WaitForSeconds(0.05f); // 50ms = 0.05 seconds
         }
     }
+
     void LoadLevel1()
     {
         SceneManager.LoadScene("Level1-Caleb");
     }
+
     void LoadLevel2()
     {
         SceneManager.LoadScene("Level2-Jacqueline");
     }
+
     void LoadLevel3()
     {
         SceneManager.LoadScene("Level3-Daniel");
     }
+
     void LoadMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
