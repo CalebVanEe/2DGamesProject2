fc3c59272f82f84a543c778f28f56bfb9cda86b4
CalebVanEe <VanEeCM23@gcc.edu>
2025-11-06 12:02:15 -0500

    Added lives and timer to level 1

--- Assets/Scripts/LevelSceneManagerScript.cs
+++ Assets/Scripts/LevelSceneManagerScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelSceneManagerScript : MonoBehaviour
@@ -6,8 +7,12 @@ public class LevelSceneManagerScript : MonoBehaviour
     public Vector3 Door1Location = new Vector3(153f, 13, 0);
     public playerScript player;
     public GameObject door;
+    public TMP_Text timerText;
     private Rigidbody2D _playerbody;
     private bool level1KeyCollected;
+    private int totalTime;
+    private float lastLevelTime;
+    private float startTime;
     private bool openingDoor;
     public int level;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,12 +22,29 @@ public class LevelSceneManagerScript : MonoBehaviour
         openingDoor = false;
         level1KeyCollected = false;
         door.GetComponent<Animator>().enabled = false;
+        lastLevelTime = PlayerPrefs.GetFloat("LevelTime");
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateTimer();
+    }
+    public void updateTimer()
+    {
+        float levelTime = Time.time - startTime;
+        totalTime = Mathf.RoundToInt(lastLevelTime + levelTime);
+        int min = (totalTime / 60);
+        int sec = totalTime % 60;
+        if (sec < 10)
+        {
+            timerText.text = min.ToString() + ":0" + sec.ToString();
+        }
+        else
+        {
+            timerText.text = min.ToString() + ":" + sec.ToString();
+        }
     }
     public void RespawnLevel()
     {
@@ -32,9 +54,19 @@ public class LevelSceneManagerScript : MonoBehaviour
     }
     public void PlayerHit()
     {
-        if (level == 1)
-            player.transform.position = Level1Spawn;
-        _playerbody.linearVelocity = Vector2.zero;
+        PlayerPrefs.SetFloat("LevelTime", totalTime);
+
+        int previousLives = PlayerPrefs.GetInt("Lives");
+        if (previousLives <= 1)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+        else
+        {
+
+            PlayerPrefs.SetInt("Lives", (previousLives - 1));
+            SceneManager.LoadScene("DeathScene");
+        }
     }
     public void OpenAttempt(bool open)
     {
